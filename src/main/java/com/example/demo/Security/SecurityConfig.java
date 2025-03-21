688d11b79e629520491d7735a01398cc9a5c565f
Jayshree2004 <jayshree.r2022cse@sece.ac.in>
2025-03-21 10:56:28 +0530

    Initial commit

--- src/main/java/com/example/demo/Security/SecurityConfig.java
+++ src/main/java/com/example/demo/Security/SecurityConfig.java
@@ -3,23 +3,28 @@ package com.example.demo.Security;  // Add this at the top
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/user/details").authenticated()
-                        .anyRequest().permitAll()
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/user/details","user/create","user/delete","user/update").authenticated() // Require authentication for this endpoint
+                        .anyRequest().permitAll() // Allow all other requests
                 )
-                .httpBasic(Customizer.withDefaults());
+                .httpBasic(HttpBasicConfigurer::disable);  // Enable basic authentication
 
         return http.build();
     }
+
 }
